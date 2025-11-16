dcb709458d951790b6ba6f8fb5f5b3e01db2b7e3
danielslloyd <danielslloyd@users.noreply.github.com>
2025-11-16 17:05:15 -0500

    Delete rewriter directory

--- rewriter/launcher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Text Rewriter Application Launcher for Windows
-REM Double-click this file to start the application
-
-cd /d "%~dp0"
-
-python launcher.py
-
-pause
--- rewriter/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Text Rewriter Application Launcher for Unix/Linux/Mac
-# Make executable with: chmod +x launcher.sh
-# Then run with: ./launcher.sh
-
-cd "$(dirname "$0")"
-
-python3 launcher.py
