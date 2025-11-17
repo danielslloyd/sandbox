0f47ffd8956c6b313d688507ecde3ce1844d45ab
danielslloyd <danielslloyd@users.noreply.github.com>
2025-11-17 07:46:56 -0500

    Delete food-nutrition-scraper directory

--- food-nutrition-scraper/scrapers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Scrapers module for various food retailers.
-"""
-
-from .base_scraper import BaseFoodScraper
-from .costco_scraper import CostcoScraper
-
-__all__ = ['BaseFoodScraper', 'CostcoScraper']
--- food-nutrition-scraper/view_results.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Open the visualization in your default browser
-
-echo Opening visualization...
-start static\index.html
